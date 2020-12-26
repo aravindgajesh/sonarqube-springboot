f313c664b301afd72155d863e67b68d17168e7a2
AravindGajesh <AravindGajesh@192.168.0.10>
2020-12-26 22:16:02 +0530

    1st sonarqube test exmaple

---
+++ src/main/java/com/ecofreeze/sonarqube/Controller/MyClass.java
@@ -0,0 +1,19 @@
+package com.ecofreeze.sonarqube.Controller;
+
+public class MyClass {
+
+	private String TEST="";
+	
+	MyClass(){
+		
+	}
+	
+	public void dummy(int myNum) {
+		myNum = 10;
+		
+		if(myNum == 0) {
+			System.out.println("Number is zero");
+		}
+	}
+	
+}
---
+++ src/main/java/com/ecofreeze/sonarqube/SonarqubeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ecofreeze.sonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeSpringBootApplication.class, args);
+	}
+
+}
